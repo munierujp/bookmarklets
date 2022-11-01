b0c1fb25141583ad193e126f38de06c0912feebd
Munieru <20086673+munierujp@users.noreply.github.com>
2022-11-01 19:35:04 +0900

    Revert "Update rollup config"
    This reverts commit 87fae37b3ac451ad1e6dca1a19a100da6bdfde12.

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,40 +6,20 @@ import watch from 'rollup-plugin-watch'
 import bookmarklet from './plugins/bookmarklet'
 
 const entryPaths = glob.sync('src/**/main.ts')
-const configs: RollupOptions[] = entryPaths.flatMap(entryPath => {
-  const mainConfig: RollupOptions = {
-    input: entryPath,
-    output: {
-      file: entryPath.replace(/^src\//, 'dist/').replace(/\/(.+)\/main\.ts$/, '/$1.js'),
-      format: 'iife'
-    },
-    plugins: [
-      typescript(),
-      bookmarklet(),
-      watch({
-        dir: 'src'
-      })
-    ]
-  }
-  const minConfig: RollupOptions = {
-    input: entryPath,
-    output: {
-      file: entryPath.replace(/^src\//, 'dist/').replace(/\/(.+)\/main\.ts$/, '/$1.min.js'),
-      format: 'iife'
-    },
-    plugins: [
-      typescript(),
-      terser(),
-      bookmarklet(),
-      watch({
-        dir: 'src'
-      })
-    ]
-  }
-  return [
-    mainConfig,
-    minConfig
+const configs: RollupOptions[] = entryPaths.map(entryPath => ({
+  input: entryPath,
+  output: {
+    file: entryPath.replace(/^src\//, 'dist/').replace(/\/(.+)\/main\.ts$/, '/$1.min.js'),
+    format: 'iife'
+  },
+  plugins: [
+    typescript(),
+    terser(),
+    bookmarklet(),
+    watch({
+      dir: 'src'
+    })
   ]
-})
+}))
 
 export default configs
