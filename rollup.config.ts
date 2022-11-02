05daa6a4219fad3d5a4519c5c90aa9b02fc4eb08
Munieru <20086673+munierujp@users.noreply.github.com>
2022-11-02 21:47:16 +0900

    Use rollup-plugin-bookmarklet (#12)
    * Add rollup-plugin-bookmarklet
    
    * Use rollup-plugin-bookmarklet
    
    * Remove bookmarklet.ts

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,7 +2,7 @@ import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 import glob from 'glob'
 import type { RollupOptions } from 'rollup'
-import bookmarklet from './plugins/bookmarklet'
+import bookmarklet from 'rollup-plugin-bookmarklet'
 
 const entryPaths = glob.sync('src/**/main.ts')
 const configs: RollupOptions[] = entryPaths.map(entryPath => ({
