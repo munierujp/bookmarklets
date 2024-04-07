2025c287da6283220c2f5ef811a138e53380cda0
Munieru <20086673+munierujp@users.noreply.github.com>
2024-04-07 16:18:45 +0900

    Fix ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,6 @@
  * @see https://eslint.org/docs/latest/use/configure/configuration-files
  */
 const config = {
-  ignorePatterns: [
-    'dist/**/*.js'
-  ],
   extends: [
     '@munierujp/eslint-config-typescript',
     'plugin:jest/recommended'
@@ -19,7 +16,17 @@ const config = {
   },
   rules: {
     'unicorn/prefer-top-level-await': 'off'
-  }
+  },
+  ignorePatterns: [
+    'dist/**/*.js'
+  ],
+  overrides: [
+    // https://typescript-eslint.io/troubleshooting/#i-get-errors-telling-me-eslint-was-configured-to-run--however-that-tsconfig-does-not--none-of-those-tsconfigs-include-this-file
+    {
+      files: ['./**/*.js'],
+      extends: ['plugin:@typescript-eslint/disable-type-checked']
+    }
+  ]
 }
 
 // eslint-disable-next-line unicorn/prefer-module
