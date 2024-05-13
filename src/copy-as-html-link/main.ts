d12487793459c9de0a5015b2071262e13ac04335
Munieru <20086673+munierujp@users.noreply.github.com>
2024-05-13 22:33:08 +0900

    Fix errors

--- src/copy-as-html-link/main.ts
+++ src/copy-as-html-link/main.ts
@@ -1,6 +1,4 @@
-import {
-  copy,
-  handleError
-} from '../util'
+import { copy } from '../util/copy.js'
+import { handleError } from '../util/handleError.js'
 
 copy(`<a href="${location.href}">${document.title}</a>`).catch(handleError)
